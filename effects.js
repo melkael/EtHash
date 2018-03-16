96b98f2feb299031c7a07347da86bacdb96cfa6b
MaxouLKL <maxime.elkael@gmail.com>
2018-03-16 11:33:16 +0100

    Separated effects from smartcontract.js

---
+++ effects.js
@@ -0,0 +1,18 @@
+function toSetup() {
+    $('html, body').animate({
+        scrollTop: $('#setup').offset().top
+    }, 'slow');
+};
+
+function toUpload() {
+    $('html, body').animate({
+        scrollTop: $('#upload').offset().top
+    }, 'slow');
+};
+
+function toVerify() {
+  console.log("verify");
+    $('html, body').animate({
+        scrollTop: $('#verify').offset().top
+    }, 'slow');
+};
